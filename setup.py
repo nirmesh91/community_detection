cfed6ac014737449f360ba10329206b164389685
Nirmesh <nirmesh91@users.noreply.github.com>
2014-10-28 21:56:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 
+
 setup(name='community_detection',
-      version='1.0',
-      description='Python Distribution Utilities',
-      author='Nirmesh Khandelwal',
-      url='http://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
+      version = '1.0',
+      description = 'Python Distribution Utilities',
+      author = 'Nirmesh Khandelwal',
+      packages = ['community_detection'],
+      install_requires = ['networkx>=1.9.1']
      )

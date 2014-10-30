f4a00592e9bc464d3ea04196a2a7a6dfaae99dd7
Nirmesh <nirmesh.khandelwal@gmail.com>
2014-10-30 18:27:10 -0400

    Initiaizing the unit testin for the project

---
+++ community_detection/test/TestCommunityDetection.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestCommunityDetection(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_weight_fuction(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
